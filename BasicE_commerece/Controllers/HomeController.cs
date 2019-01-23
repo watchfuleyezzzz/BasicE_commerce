69f95102758dac13003b98e0835c88a88b569486
Al-aminBadhon <badhonrex786@gmail.com>
2019-01-23 18:24:03 +0600

    Admin panel work

--- BasicE_commerece/Controllers/HomeController.cs
+++ BasicE_commerece/Controllers/HomeController.cs
@@ -9,9 +9,19 @@ namespace BasicE_commerece.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        Models.basicE_commerceEntities1 db = new Models.basicE_commerceEntities1();
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult Product()
+        {
+            return View(db.Products.ToList());
+        }
+        public ActionResult ProductAdd()
+        {
+            return View();
+        }
     }
+
 }
